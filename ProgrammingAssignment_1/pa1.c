b97b65e30bdfaf0088bd9e07f8acc83aff2bebe2
CRogers18 <c.rogers@knights.ucf.edu>
2018-02-11 14:37:10 -0500

    File IO
    + Added file read and option to read all files from working directory

--- ProgrammingAssignment_1/pa1.c
+++ ProgrammingAssignment_1/pa1.c
@@ -1,7 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct process
+{
+	char name[2];
+	int burstTime, arrivalTime;
+} process;
 
 int main(int argc, char const *argv[])
 {
-	/* TODO: code here */
+	char fileName[] = "set1_process.in";
+	int i, debug = 0;
+	FILE *input;
+
+	if (debug)
+	{
+		for (i = 1; i < 5; i++)
+		{
+			input = fopen(fileName, "r");
+
+			if (input == NULL)
+			{
+				printf("file %s not found!\n", fileName);
+				return -1;
+			}
+
+			char *lineBuffer = malloc(128);
+			while(fgets(lineBuffer, 128, input) != NULL)
+			{
+				printf("%s", lineBuffer);
+			}
+
+			fclose(input);
+			fileName[3] = i + '0';
+		}
+	}
+
+	else
+	{
+		input = fopen(fileName, "r");
+
+		if (input == NULL)
+		{
+			printf("file %s not found!\n", fileName);
+			return -1;
+		}
+
+		char *lineBuffer = malloc(128);
+		while(fgets(lineBuffer, 128, input) != NULL)
+		{
+			printf("%s", lineBuffer);
+		}
+
+		fclose(input);
+	}
+
+//	process *processArray = malloc(sizeof(process) * 2);
+	
 	return 0;
 }
