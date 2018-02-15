f9d51d3ffaad1137b730542cf9e296e6789324d2
CRogers18 <c.rogers@knights.ucf.edu>
2018-02-14 22:25:09 -0500

    FCFS Complete
    Added function for FCFS algorithm using hard-coded values

--- ProgrammingAssignment_1/pa1.c
+++ ProgrammingAssignment_1/pa1.c
@@ -1,19 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define INT_MAX 2147483647
 
 typedef struct process
 {
-	char name[2];
+	char *name;
 	int burstTime, arrivalTime;
 } process;
+
+void printList(process *procList, int numProc)
+{
+	int i;
+
+	for(i = 0; i < numProc; i++)
+		printf("%s : %d\t", procList[i].name, procList[i].arrivalTime);
+
+	printf("\n");
+}
+
+process *sortProcessList(process *procList, int numProc)
+{
+	int i, j;
+
+//	printf("Before sorting...\n");
+//	printList(procList, numProc);
+
+	for(i = 0; i < numProc; i++)
+	{
+		for(j = i+1; j < numProc; j++)
+		{
+			if(procList[i].arrivalTime > procList[j].arrivalTime)
+			{
+				process temp = procList[i];
+				procList[i] = procList[j];
+				procList[j] = temp;
+			}
+		}
+	}
+
+//	printf("After sorting...\n");
+//	printList(procList, numProc);
+
+	return procList;
+}
+
+void runFCFS(process *procList, int duration, int numProcesses)
+{
+	int time = 0, isDone = 0, nextArrival = 0, i = 0, j = 0;
+	int arrivalTimes[numProcesses];
+	procList = sortProcessList(procList, numProcesses);
+	process activeProc;
+
+	for (i = 0; i < numProcesses; i++)
+		arrivalTimes[i] = procList[i].arrivalTime;
+
+	printf("\n\n%d processes\nUsing First Come First Served\n", numProcesses);
+
+	while(!isDone)
+	{
+		for(i = 0; i < numProcesses; i++)
+		{
+			activeProc = procList[i];
+
+			if(i == 0)
+			{
+				printf("Time %d : %s arrived\n", time, procList[j].name);
+				j++;
+			}
+
+			printf("Time %d : %s selected (burst %d)\n", time, activeProc.name, activeProc.burstTime);
+
+			while(activeProc.burstTime != 0)
+			{
+				if(time == arrivalTimes[j])
+				{
+					printf("Time %d : %s arrived\n", time, procList[j].name);
+					
+					if((j + 1) != numProcesses)
+						j++;
+				}
+
+				activeProc.burstTime--;
+				time++;
+			}
+
+			printf("Time %d : %s finished\n", time, activeProc.name);
+		}
+
+		isDone = 1;
+		printf("Finished at time %d\n", time);
+	}
+
+}
 
 int main(int argc, char const *argv[])
 {
 	char fileName[] = "set1_process.in";
-	int i, debug = 0;
+	int i, runAll = 0;
 	FILE *input;
 
-	if (debug)
+	if (runAll)
 	{
 		for (i = 1; i < 5; i++)
 		{
@@ -55,7 +143,25 @@ int main(int argc, char const *argv[])
 		fclose(input);
 	}
 
-//	process *processArray = malloc(sizeof(process) * 2);
+	// Some temporary processes for testing
+	process *procList = malloc(sizeof(process) * 3);
+	
+	procList[0].arrivalTime = 0;
+	procList[0].burstTime = 24;
+	procList[0].name = malloc(sizeof(char) * 2);
+	strcpy(procList[0].name, "P1");
+	
+	procList[1].arrivalTime = 3;
+	procList[1].burstTime = 3;
+	procList[1].name = malloc(sizeof(char) * 2);
+	strcpy(procList[1].name, "P2");
+
+	procList[2].arrivalTime = 4;
+	procList[2].burstTime = 3;
+	procList[2].name = malloc(sizeof(char) * 2);
+	strcpy(procList[2].name, "P3");
+
+	runFCFS(procList, 30, 3);
 	
 	return 0;
 }
