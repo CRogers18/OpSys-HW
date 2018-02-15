5a6cc59871a2775fa43be8bcd1b97e2d42d62869
AlexisDrayton <a.drayotn1313@gmail.com>
2018-02-15 00:37:03 -0500

    Added File Input. Need to add printing to out file, rr and sjf.

--- ProgrammingAssignment_1/pa1.c
+++ ProgrammingAssignment_1/pa1.c
@@ -56,7 +56,7 @@ void runFCFS(process *procList, int duration, int numProcesses)
 	for (i = 0; i < numProcesses; i++)
 		arrivalTimes[i] = procList[i].arrivalTime;
 
-	printf("\n\n%d processes\nUsing First Come First Served\n", numProcesses);
+	printf("%d processes\nUsing First Come First Served\n\n", numProcesses);
 
 	while(!isDone)
 	{
@@ -97,19 +97,27 @@ void runFCFS(process *procList, int duration, int numProcesses)
 
 int main(int argc, char const *argv[])
 {
-	char fileName[] = "set1_process.in";
-	int i, runAll = 0;
-	FILE *input;
+	char inFilename[] = "set1_process.in";
+	char outFilename[] = "processes.out";
+	int i = 0, j = 0, h = 0, debug = 0, linenumber = 1, x = 0;
+	FILE *input, *output;
+	// Process variables
+	int processCount, runfor, quantum; 
+	char use[5]; //Holds the algorithm type
+	char temp[10]; //Temp array to convert the input characters to integers 
+	char chtemp[2];
+	char lineBuffer[1024]; 
+	int arrivalTimes[1024], burstTimes[1024];
 
-	if (runAll)
+	/*if (debug)
 	{
 		for (i = 1; i < 5; i++)
 		{
-			input = fopen(fileName, "r");
+			input = fopen(inFilename, "r");
 
 			if (input == NULL)
 			{
-				printf("file %s not found!\n", fileName);
+				printf("file %s not found!\n", inFilename);
 				return -1;
 			}
 
@@ -120,33 +128,157 @@ int main(int argc, char const *argv[])
 			}
 
 			fclose(input);
-			fileName[3] = i + '0';
+			inFilename[3] = i + '0';
 		}
-	}
+	}*/
 
-	else
-	{
-		input = fopen(fileName, "r");
+/*	else
+	{*/
+		input = fopen(inFilename, "r");
+		// TO DO: Do output file stuff in the Algorithm functions where the print statments are
+//		output = fopen(outFilename, "w");
 
 		if (input == NULL)
 		{
-			printf("file %s not found!\n", fileName);
+			printf("file %s not found!\n", inFilename);
 			return -1;
 		}
 
-		char *lineBuffer = malloc(128);
+		
 		while(fgets(lineBuffer, 128, input) != NULL)
-		{
-			printf("%s", lineBuffer);
+		{	// Parse Through each line
+			// printf("Line number = %d: %s", linenumber,lineBuffer);
+			// Clear temporary values
+			j = 0;
+			x = 0;
+			memset(&temp[0], 0, sizeof(temp));
+			// Read until comment
+			if(linenumber <= 4){ 
+				while(lineBuffer[j] != '#'){
+					//Process count always on first line
+					if((lineBuffer[j] == ' ') && linenumber == 1){
+						while(lineBuffer[j + 1] != ' '){
+							// Until next space grab the number
+							temp[x] = lineBuffer[j + 1];
+							x++;
+							j++;
+						}
+						// Convert char array of numbers to an integer
+						sscanf(temp, "%d", &processCount);
+						//printf("%d Processes\n", processCount);
+						break;
+					}
+					// time cycles on line 2
+					if((lineBuffer[j] == ' ') && linenumber == 2){
+						while(lineBuffer[j + 1] != ' '){
+							// Until next space grab the number
+							temp[x] = lineBuffer[j + 1];
+							x++;
+							j++;
+						}
+						sscanf(temp, "%d", &runfor);
+						//printf("Run for %d time cycles\n", runfor);	
+						break;
+					}
+					if((lineBuffer[j] == ' ') && linenumber == 3){
+						while(lineBuffer[j + 1] != ' '){
+							// Until next space grab the number
+							use[x] = lineBuffer[j + 1];
+							x++;
+							j++;
+						}
+					
+						//printf("Using %s\n", use);	
+						break;
+					}
+					// line four can either have the quantum or a process input
+					if(linenumber == 4 && lineBuffer[j] == ' '){
+						while(lineBuffer[j + 1] != ' '){
+						// Until next space grab the number
+						temp[x] = lineBuffer[j + 1];
+						x++;
+						j++;
+					}
+						sscanf(temp, "%d", &quantum);
+						//printf("Quantum = %d\n", quantum);	
+						break;
+					}
+					
+
+					j++;
+				}
+		}
+		// Parse the line buffer for Process information
+		else{			
+			while(lineBuffer[j] != '\n'){
+				x = 0;
+				memset(&temp[0], 0, sizeof(temp));
+				//Grab the arrival number after the word arriva'l'
+				if(lineBuffer[j] == 'l'){
+					j = j + 1;
+					while(lineBuffer[j + 1] != ' '){
+						// Until next space grab the number
+						temp[x] = lineBuffer[j + 1];
+						x++;
+						j++;
+					}
+					sscanf(temp, "%d", &arrivalTimes[i]);
+					//printf("For process %d arrival time is %d\n", i + 1, arrivalTimes[i]);
+					i++;
+				}
+				// Grab the burst number after the word burs't'
+				if(lineBuffer[j] == 't'){
+					j = j + 1;
+					while(lineBuffer[j + 1] != ' '){
+						// Until next space grab the number
+						temp[x] = lineBuffer[j + 1];
+						x++;
+						j++;
+					}
+					sscanf(temp, "%d", &burstTimes[h]);
+					//printf("For process %d burst length is %d\n", h + 1, burstTimes[h]);
+					h++;
+				}	
+				j++;		
+			}
 		}
 
-		fclose(input);
+		linenumber++;
 	}
 
-	// Some temporary processes for testing
-	process *procList = malloc(sizeof(process) * 3);
-	
-	procList[0].arrivalTime = 0;
+		fclose(input);
+	/*}*/
+
+	process *procList = malloc(sizeof(process) * processCount);
+
+	//Populate the Processes Array
+
+	chtemp[0] = 'P';
+	for(i = 0; i < processCount; i++){
+		procList[i].arrivalTime = arrivalTimes[i];
+		procList[i].burstTime = burstTimes[i];
+		procList[i].name = malloc(sizeof(char) * 2);
+		chtemp[1] = (i+1) + '0';
+		strcpy(procList[i].name, chtemp);
+		//printf("%s\n",procList[i].name);
+	}
+
+	// Send to Scheduling Algorithms 
+	// If First Come First Serve
+	if(use[0] == 'f'){
+
+		runFCFS(procList, runfor, processCount);
+	}
+	else if(use[0] == 'r'){
+		// TO DO: write round robin
+		printf("use Round Robin\n");
+	}
+	else if(use[0] == 's'){
+		// TO DO: write shortest job first
+		printf("use Shortest Job First\n");
+	}
+
+/*	procList[0].arrivalTime = 0;
 	procList[0].burstTime = 24;
 	procList[0].name = malloc(sizeof(char) * 2);
 	strcpy(procList[0].name, "P1");
@@ -161,7 +293,7 @@ int main(int argc, char const *argv[])
 	procList[2].name = malloc(sizeof(char) * 2);
 	strcpy(procList[2].name, "P3");
 
-	runFCFS(procList, 30, 3);
+	runFCFS(procList, 30, 3);*/
 	
 	return 0;
 }
