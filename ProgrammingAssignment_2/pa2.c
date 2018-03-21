9f50e961d5c681c329bd99f2f29f8dc6f3f8b98c
ryanalfred119 <36483452+ryanalfred119@users.noreply.github.com>
2018-03-21 17:34:10 -0400

    Update pa2.c

--- ProgrammingAssignment_2/pa2.c
+++ ProgrammingAssignment_2/pa2.c
@@ -3,9 +3,39 @@
 #define BUFFER_SIZE 1024;
 static char inputBuffer[BUFFER_SIZE];
 
+void readAndRemove (char inputBuffer);
+
+void readAndRemove (char inputBuffer)
+{
+	int i = 0;
+	
+	//empty input condition
+	if(inputBuffer[0] == NULL)
+	{
+		printf("No Input Found!\n");
+	}
+	
+	//until it hits a null terminator in the string, it prints out each character and turns it into a null terminator
+	while(inputBuffer[i] != NULL)
+	{
+		printf("%c", inputBuffer[i]);
+		
+		inputBuffer[i] == NULL;
+		
+		i++;
+	}
+}
+
 int main()
 {
-	printf("Yo\n");
+	inputBuffer = {NULL};
 	
+	//input goes into intputBuffer here
+	
+	//if the user wants this to happen
+	readAndRemove(inputBuffer);
+	
+	printf("Yo\n");
+
 	return 0;
 }
