f6ad7654b51cb4820fb82a2a2fe88946f09cef5b
ryanalfred119 <36483452+ryanalfred119@users.noreply.github.com>
2018-04-05 22:12:36 -0400

    Added mutex

--- ProgrammingAssignment_3/CharBufferInput.c
+++ ProgrammingAssignment_3/CharBufferInput.c
@@ -17,6 +17,7 @@ static int dev_open(struct inode*, struct file*);
 static int dev_release(struct inode*, struct file*);
 static ssize_t dev_read(struct file*, char*, size_t, loff_t*);
 static ssize_t dev_write(struct file*, const char*, size_t, loff_t*);
+static DEFINE_MUTEX(input_mutex);
  
 static struct file_operations fileOps =
 {
@@ -28,6 +29,8 @@ static struct file_operations fileOps =
  
 int init_module(void)
 {
+	mutex_init(&input_mutex);
+	
     int num = register_chrdev(42,"NSACharBuffer", &fileOps);
  
     if(num < 0)
@@ -62,6 +65,12 @@ static ssize_t dev_write(struct file *filp, const char* buff, size_t len, loff_t
             printk(KERN_INFO "[ERROR] Buffer full, %d bytes were written.\n", bytesWritten);
             return len;
         }
+		
+		if(!mutex_trylock(&input_mutex))
+		{
+			printk(KERN_ALERT "Device is in use by another process!");
+			return len;
+		}
  
         else
         {
@@ -77,6 +86,11 @@ static ssize_t dev_write(struct file *filp, const char* buff, size_t len, loff_t
  
 static int dev_release(struct inode* inod, struct file* fil)
 {
+	mutex_unlock(&input_mutex);
+	
     printk(KERN_INFO "[RELEASE] NSACharBuffer Device was closed... maybe...\n");
+	
+	mutex_destroy(&input_mutex);
+	
     return 0;
 }
